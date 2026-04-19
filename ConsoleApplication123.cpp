250a543175c09c3fed5fbec9a6bcd25b06e13655
nasty220astajk-design <nasty220astajk@gmail.com>
2026-04-19 17:39:25 +1000

    доксифайл

---
+++ ConsoleApplication123.cpp
@@ -0,0 +1,44 @@
+﻿#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+/**
+ * @file ConsoleApplication23.cpp
+ * @brief Программа для расчета тригонометрических функций.
+ */
+
+ /**
+  * @brief Главная функция программы.
+  * @details Считывает угол в градусах из стандартного ввода,
+  * переводит его в радианы и вычисляет sin, cos, tan.
+  *
+  * @return int Статус завершения программы (0 - успех).
+  */
+int main() {
+	/** @brief Переменная для хранения введенного угла в градусах. */
+	double a;
+
+	/** @brief Синус угла. */
+	double b;
+
+	/** @brief Косинус угла. */
+	double c;
+
+	/** @brief Тангенс угла. */
+	double d;
+
+	cin >> a;
+
+	// Перевод градусов в радианы и вычисление
+	b = sin(a * (M_PI / 180));
+	c = cos(a * (M_PI / 180));
+	d = tan(a * (M_PI / 180));
+
+	cout << "sin: " << b << endl <<
+		"cos: " << c << endl <<
+		"tan: " << d << endl;
+
+	return 0;
+}
---
+++ html/search/all_3.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['readme_2emd_0',['README.md',['../_r_e_a_d_m_e_8md.html',1,'']]],
+  ['repository_5f4_1',['Repository_4',['../md__r_e_a_d_m_e.html',1,'']]]
+];
